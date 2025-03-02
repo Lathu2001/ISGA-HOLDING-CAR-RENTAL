4b288c9962af6c7a9cb6dd813f5019366f476c1c
Lathurjan Thavapiragasam <165404387+Lathu2001@users.noreply.github.com>
2025-03-02 19:47:51 +0530

    initial coding

--- ISGA-HOLDING-backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
-const router = express.Router();
-
-// Register route
-router.post('/register', registerUser);  // Register route
-router.post('/login', loginUser);        // Login route
-
-module.exports = router;
